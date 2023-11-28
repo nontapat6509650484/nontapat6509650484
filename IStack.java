c879c438097166b664761b1d95b8760d2d9f518c
nontapat6509650484 <144870873+nontapat6509650484@users.noreply.github.com>
2023-11-28 10:42:44 +0700

    Add files via upload

---
+++ IStack.java
@@ -0,0 +1,12 @@
+package core;
+public interface IStack {
+	
+	int getsize();
+	boolean IsEmpty();
+	void push(Object elm)throws Exception;
+	
+	Object top();
+	boolean IsFull();
+	
+	Object pop();
+}
